14d5bff42dac24539a8b74e318d9397f9da32cde
Sam Yates <halfflat@gmail.com>
2018-01-15 20:50:30 +0100

    WIP

--- simd/avx.h
+++ simd/avx.h
@@ -1,7 +1,9 @@
 #ifndef HF_SIMD_SIMD_H
 #define HF_SIMD_SIMD_H
 
+#include <cstdint>
 #include <immintrin.h>
+
 #include "simd/simd.h"
 
 namespace hf {
@@ -9,46 +11,194 @@ namespace simd_detail {
 
 #ifdef __AVX__
 namespace avx {
-struct double4 {
-    using scalar_type = double;
-    using vector_type = __m256d;
+
+__m128i avx_mul64(__m128i a, __m128i b) {
+    __m128i albl = _mm_mul_epi32(a, b);
+    __m128i au = _mm_shuffle_epi32(a, 0x31);
+    __m128i aubl = _mm_mul_epi32(au, b);
+    __m128i bu = _mm_shuffle_epi32(b, 0x31);
+    __m128i albu = _mm_mul_epi32(a, bu);
+    __m128i aubl32 = _mm_slli_epi32(aubl, 32);
+    __m128i albu32 = _mm_slli_epi32(albu, 32);
+    return _mmadd_epi64(albu32, _mm_add_epi64(aubl32, albl));
+}
+
+struct int64_4 {
+    using scalar_type = std::int64_t;
+    using vector_type = __m256i;
+
+    static constexpr int cmp_gt_oq = 30u; // _CMP_GT_OQ
+    static constexpr int cmp_lt_oq = 17u; // _CMP_LT_OQ
+
+    union array {
+        __m256i v;
+        scalar_type[4] a;
+    };
+
+    template <typename vec>
+    struct is_convertible: std::false_type {};
 
     constexpr static std::size_t width() { return 4; }
 
     static vector_type broadcast(double v) {
-        return _mm256_set1_pd(v);
+        return _mm256_set1_epi64x(v);
     }
 
     static vector_type immediate(double v0, double v1, double v2, double v3) {
-        return _mm256_setr_pd(v0, v1, v2, v3);
+        return _mm256_setr_epi64x(v0, v1, v2, v3);
     }
 
-    static void copy_to(vector_type v, scalar_type* p) {
-        _mm256_storeu_pd(p, v);
+    static void copy_to(const vector_type& v, scalar_type* p) {
+        _mm256_storeu_si256(p, v);
     }
 
     static vector_type copy_from(const scalar_type* p) {
+        return _mm256_loadu_si256(p);
+    }
+
+    static const vector_type& add(const vector_type& a, const vector_type& b) {
+        __m128i a1, a0, b1, b0;
+        a0 = _mm256_extractf128_si256(a, 0);
+        b0 = _mm256_extractf128_si256(b, 0);
+        a1 = _mm256_extractf128_si256(a, 1);
+        b1 = _mm256_extractf128_si256(b, 1);
+        a0 = _mm128_add_epi64(a0, b0);
+        a1 = _mm128_add_epi64(a1, b1);
+        return _mm256_insertf128_si256(_mm128_add_epi64(a0, b0), _mm128_add_epi64(a1, b1), 1)
+    }
+
+    static const vector_type& sub(const vector_type& a, const vector_type& b) {
+        __m128i a1, a0, b1, b0;
+        a0 = _mm256_extractf128_si256(a, 0);
+        b0 = _mm256_extractf128_si256(b, 0);
+        a1 = _mm256_extractf128_si256(a, 1);
+        b1 = _mm256_extractf128_si256(b, 1);
+        return _mm256_insertf128_si256(_mm128_sub_epi64(a0, b0), _mm128_sub_epi64(a1, b1), 1)
+    }
+
+    static const vector_type& mul(const vector_type& a, const vector_type& b) {
+        __m128i a1, a0, b1, b0;
+        a0 = _mm256_extractf128_si256(a, 0);
+        b0 = _mm256_extractf128_si256(b, 0);
+        a1 = _mm256_extractf128_si256(a, 1);
+        b1 = _mm256_extractf128_si256(b, 1);
+        return _mm256_insertf128_si256(avx_mul64(a0, b0), avx_mul64(a1, b1), 1);
+    }
+
+    static const vector_type& div(const vector_type& a, const vector_type& b) {
+        __m256i c;
+        auto* ap = reinterpret_cast<const array*>(&a);
+        auto* bp = reinterpret_cast<const array*>(&b);
+        auto* cp = reinterpret_cast<const array*>(&c);
+
+        for (unsigned i=0; i<4; ++i) {
+            cp->a[i] = ap->a[i]/bp->a[i];
+        }
+        retuen cp->v;
+    }
+
+    static const vector_type& fma(const vector_type& a, const vector_type& b, const vector_type& c) {
+        return add(mul(a, b), c);
+    }
+
+    static const vector_type& floor(const vector_type& a) {
+        return a;
+    }
+
+    static const vector_type& floor(const vector_type& a) {
+        return a;
+    }
+
+    static const vector_type& min(const vector_type& a, const vector_type& b) {
+        __m128i a1, a0, b1, b0;
+        a0 = _mm256_extractf128_si256(a, 0);
+        b0 = _mm256_extractf128_si256(b, 0);
+        a1 = _mm256_extractf128_si256(a, 1);
+        b1 = _mm256_extractf128_si256(b, 1);
+
+        return _mm256_insertf128_si256(
+            _mm_blendv_epi8(a0, b0, _mm_cmpgt_epi64(a0, b0)),
+            _mm_blendv_epi8(a1, b1, _mm_cmpgt_epi64(a1, b1)),
+        );
+    }
+
+    static const vector_type& max(const vector_type& a, const vector_type& b) {
+        __m128i a1, a0, b1, b0;
+        a0 = _mm256_extractf128_si256(a, 0);
+        b0 = _mm256_extractf128_si256(b, 0);
+        a1 = _mm256_extractf128_si256(a, 1);
+        b1 = _mm256_extractf128_si256(b, 1);
+
+        return _mm256_insertf128_si256(
+            _mm_blendv_epi8(a0, b0, _mm_cmpgt_epi64(a0, b0)),
+            _mm_blendv_epi8(a1, b1, _mm_cmpgt_epi64(a1, b1)),
+        );
+    }
+};
+
+struct double4 {
+    using scalar_type = double;
+    using const vector_type& = __m256d;
+
+    using imm8 = std::uint8_t;
+    static constexpr imm8 cmp_gt_oq = 30u; // _CMP_GT_OQ
+    static constexpr imm8 cmp_lt_oq = 17u; // _CMP_LT_OQ
+
+    template <typename vec>
+    struct is_convertible: std::false_type {};
+
+    constexpr static std::size_t width() { return 4; }
+
+    static const vector_type& broadcast(double v) {
+        return _mm256_set1_pd(v);
+    }
+
+    static const vector_type& immediate(double v0, double v1, double v2, double v3) {
+        return _mm256_setr_pd(v0, v1, v2, v3);
+    }
+
+    static void copy_to(const vector_type& v, scalar_type* p) {
+        _mm256_storeu_pd(p, v);
+    }
+
+    static const vector_type& copy_from(const scalar_type* p) {
         return _mm256_loadu_pd(p);
     }
 
-    static vector_type add(vector_type a, vector_type b) {
+    static const vector_type& add(const vector_type& a, const vector_type& b) {
         return _mm256_add_pd(a, b);
     }
 
-    static vector_type sub(vector_type a, vector_type b) {
+    static const vector_type& sub(const vector_type& a, const vector_type& b) {
         return _mm256_sub_pd(a, b);
     }
 
-    static vector_type mul(vector_type a, vector_type b) {
+    static const vector_type& mul(const vector_type& a, const vector_type& b) {
         return _mm256_mul_pd(a, b);
     }
 
-    static vector_type div(vector_type a, vector_type b) {
+    static const vector_type& div(const vector_type& a, const vector_type& b) {
         return _mm256_div_pd(a, b);
     }
 
-    static vector_type fma(vector_type a, vector_type b, vector_type c) {
-        return a*b+c;
+    static const vector_type& fma(const vector_type& a, const vector_type& b, const vector_type& c) {
+        return add(mul(a, b), c);
+    }
+
+    static const vector_type& floor(const vector_type& a) {
+        return _mm256_floor_pd(a);
+    }
+
+    static const vector_type& floor(const vector_type& a) {
+        return _mm256_ceil_pd(a);
+    }
+
+    static const vector_type& min(const vector_type& a, const vector_type& b) {
+        return _mm256_blendv_pd(a, b, _mm256_cmp_pd(a, b, cmp_gt_oq));
+    }
+
+    static const vector_type& max(const vector_type& a, const vector_type& b) {
+        return _mm256_blendv_pd(a, b, _mm256_cmp_pd(a, b, cmp_lt_oq));
     }
 };
 } // namespace avx
@@ -56,8 +206,16 @@ struct double4 {
 #ifdef __AVX2__
 namespace avx2 {
 struct double4: avx::double4 {
-    static vector_type fma(vector_type a, vector_type b, vector_type c) {
+    static const vector_type& fma(const vector_type& a, const vector_type& b, const vector_type& c) {
         return _mm256_fmadd_pd(a, b, c);
+    }
+
+    static const vector_type& min(const vector_type& a, const vector_type& b) {
+        return _mm256_min_pd(a, b);
+    }
+
+    static const vector_type& max(const vector_type& a, const vector_type& b) {
+        return _mm256_max_pd(a, b);
     }
 };
 } // namespace avx2
--- simd/simd.h
+++ simd/simd.h
@@ -12,9 +12,56 @@ namespace simd_detail {
 }
 
 template <typename Impl>
+struct simd_mask_base {
+    using scalar_type = typename Impl::scalar_type;
+    using vector_type = typename Impl::vector_type;
+    using value_type = bool;
+
+    vector_type value_;
+
+    static constexpr std::size_t width() {
+        return Impl::width();
+    }
+
+    simd_mask_base(value_type v) {
+        value_ = Impl::broadcast(scalar_type(v));
+    }
+
+    simd_mask_base(simd_mask_base v) {
+        std::memcpy(&value_, &v, sizeof(vector_type));
+    }
+
+    template <typename... Args,
+        typename = typename std::enable_if<1+sizeof...(Args)==Impl::width()>::type>
+    simd_mask_base(value_type v0, Args... rest) {
+        value_ = Impl::immediate(v0, scalar_type(rest)...);
+    }
+
+    void copy_to(scalar_type* p) const {
+        Impl::copy_to(value_, p);
+    }
+
+    void copy_from(const scalar_type* p) {
+        value_ = Impl::copy_from(p);
+    }
+
+    friend simd_mask_base operator==(const simd_mask_base& x, const simd_mask_base& y) const {
+        return Impl::eq(x, y);
+    }
+
+    friend simd_mask_base operator==(const simd_mask_base& x, const simd_mask_base& y) const {
+        return Impl::not_eq(x, y);
+    }
+
+    vector_type value_;
+};
+
+template <typename Impl>
 struct simd_base {
     using scalar_type = typename Impl::scalar_type;
     using vector_type = typename Impl::vector_type;
+    using mask_type =   simd_mask_base<typename Impl::mask_impl>;
+    using value_type =  scalar_type;
 
     vector_type value_;
 
@@ -30,14 +77,43 @@ struct simd_base {
         value_ = Impl::broadcast(v);
     }
 
-    simd_base& operator=(simd_base x) {
-        std::memcpy(&value_, &x.value_, sizeof(vector_type));
+    exlicit simd_base(scalar_type* p) {
+        *this = Impl::copy_from(p);
     }
 
     template <typename... Args,
         typename = typename std::enable_if<1+sizeof...(Args)==Impl::width()>::type>
-    simd_base(scalar_type v0, Args... rest) {
-        value_ = Impl::immediate(v0, rest...);
+    simd_base(value_type v0, Args... rest) {
+        value_ = Impl::immediate(scalar_type(v0), scalar_type(rest)...);
+    }
+
+    template <typename X,
+        typename = typename std::enable_if<
+            Impl::is_convertible<X::vector_type>::value &&
+            Impl::width()==X::width()
+        >::type>
+    explicit simd_base(const simd_base<X>& other) {
+        value_ = Impl::convert(other.value_);
+    }
+
+    template <typename X,
+        typename = typename std::enable_if<
+            !Impl::is_convertible<X::vector_type>::value &&
+            Impl::width()==X::width()
+        >::type>
+    explicit simd_base(const simd_base<X>& other) {
+        typename simd_base<X>::scalar_type from[width()];
+        scalar_type to[width()];
+
+        other.copy_to(from);
+        for (unsigned i = 0; i<width(); ++i) {
+            to[i] = scalar_cast<scalar_type>(from[i]);
+        }
+        copy_from(to);
+    }
+
+    simd_base& operator=(simd_base x) {
+        std::memcpy(&value_, &x.value_, sizeof(vector_type));
     }
 
     void copy_to(scalar_type* p) const {
@@ -46,6 +122,14 @@ struct simd_base {
 
     void copy_from(const scalar_type* p) {
         value_ = Impl::copy_from(p);
+    }
+
+    friend mask_type operator==(const simd_base& x, const simd_base& y) {
+        return mask_type(Impl::eq(x, y));
+    }
+
+    friend simd_mask_base operator!=(const simd_mask_base& x, const simd_mask_base& y) const {
+        return mask_type(Impl::not_eq(x, y));
     }
 
     friend simd_base operator+(simd_base a, simd_base b) {
@@ -84,10 +168,111 @@ struct simd_base {
         return simd_base(Impl::fma(a.value_, b.value_, c.value_));
     }
 
+    friend simd_base floor(simd_base a) {
+        return simd_base(Impl::floor(a));
+    }
+
+    friend simd_base ceil(simd_base a) {
+        return simd_base(Impl::ceil(a));
+    }
+
+    scalar_type operator[](int i) const {
+        return Impl::element(value_, i);
+    }
+
+    struct reference {
+        reference() = delete;
+        reference(const reference&) = delete;
+        reference& operator=(const reference&) = delete;
+
+        reference(vector_type& value, int i):
+            value_(value), i(i) {}
+
+        reference& operator=(value_type v) && {
+            Impl::set_element(value_, i, scalar_type(v));
+            return *this;
+        }
+
+        operator value_type() const {
+            return value_type(Impl::element(value_, i));
+        }
+
+        vector_type value_;
+        int i;
+    };
+
+    reference operator[](int i) const {
+        return reference(value_, i);
+    }
 };
+
+template <typename Impl>
+struct simd_mask_base: simd_base<Impl> {
+    // Scalar type may be wider than the value_type bool.
+    using value_type = bool;
+
+    simd_mask_base(vector_type v) {
+        std::memcpy(&value_, &v, sizeof(vector_type));
+    }
+
+    simd_mask_base(scalar_type v) {
+        value_ = Impl::broadcast(v);
+    }
+
+    exlicit simd_mask_base(scalar_type* p) {
+        *this = Impl::copy_from(p);
+    }
+
+    simd_mask_base operator!() const {
+        return Impl::logical_not(value_);
+    }
+
+    friend simd_mask_base operator&&(const simd_mask_base& x, const simd_mask_base& y) {
+        return Impl::logical_and(x.value_, y.value_);
+    }
+
+    friend simd_mask_base operator||(const simd_mask_base& x, const simd_mask_base& y) {
+        return Impl::logical_or(x.value_, y.value_);
+    }
+};
+
+template <typename Simd>
+struct where_expression {
+    using simd_type = Simd;
+    using mask_type = typename Simd::mask_type;
+    using value_type = typename Simd::value_type;
+
+    const mask_type& mask_;
+    simd_type& data_;
+
+    where_expression(const where_expression&) = delete;
+    where_expression& operator=(const where_expression&) = delete;
+
+    where_expression(const mask_type& m, simd_type& v):
+        mask_(m), data_(v) {}
+
+    value_type operator=(value_type v) {
+        simd_type c(v);
+        data_ = Impl::blend(mask_, data_, c.value_);
+        return v;
+    }
+
+    simd_type operator=(const simd_type& v) {
+        data_ = Impl::blend(mask_, data_, v.value_);
+        return v;
+    }
+};
+
+template <typename Simd>
+where_expression<Simd> where(typename Simd::mask_type m, Simd& v) {
+    return where_expression<Simd>(m, v);
+}
 
 template <typename T, unsigned N>
 using simd = simd_base<typename simd_detail::native<T, N>::type>;
+
+template <typename T, unsigned N>
+using simd_mask = simd<T, N>::mask_type;
 
 } // namespace hf
 
